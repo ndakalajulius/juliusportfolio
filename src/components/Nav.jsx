86e80e4f4dd5668dc99bdeeb0ea9b4ed7356466e
julius ndakala <juliusndakala35@gmail.com>
2023-06-05 14:47:06 +0300

    Created navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import './Nav.css';
+
+export default function Nav({ currentPage, setCurrentPage }) {
+    return (
+        <>
+            <ul className="Nav">
+                <li onClick={() => setCurrentPage('home')}><i className="nav-icon fa-solid fa-house"></i></li>
+                <li onClick={() => setCurrentPage('resume')}><i className="nav-icon fa-solid fa-briefcase"></i></li>
+                <li onClick={() => setCurrentPage('projects')}><i className="nav-icon fa-solid fa-code"></i></li>
+                <li><a href="mailto:alexehouston@gmail.com" target="_blank" rel="noreferrer"><i className="nav-icon fa-solid fa-envelope"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/alexehouston/" target="_blank" rel="noreferrer"><i className="nav-icon fa-brands fa-linkedin"></i></a></li>
+                <li><a href="https://github.com/alexehouston" target="_blank" rel="noreferrer"><i className="nav-icon fa-brands fa-github"></i></a></li>
+            </ul>
+        </>
+    );
+}
